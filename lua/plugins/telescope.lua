981464c61e8f36796d8e12e92fabef08679316b6
Sergio Chavez <ser_chg@hotmail.com>
2023-06-17 18:21:07 -0600

    se agrega copilot y telescope con sus respectivas configuraciones

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+	{ "<leader>ff", "<cmd>Telescope find_files<cr>" },
+	{ "<C-p>", "<cmd>Telescope git_files<cr>" },
+	--{ vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>') }
+    }
+}
